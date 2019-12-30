9c78b9b51e95d2304e266d54730fb706d51a3423
liyizhe <liyizhe@kuaishou.com>
2019-12-30 18:03:03 +0800

    initial commit

---
+++ index.ts
@@ -0,0 +1,65 @@
+import Observable, { Observer } from 'lobservable'
+import MethodChain from './method-chain'
+
+export type Options = { 
+    shouldEmitUrlChange?: (oldPath: string, newPath: string) => boolean
+    trackReplaceState?: boolean
+}
+
+function getPath() {
+    const { pathname, search, hash } = location
+    return pathname + search + hash
+}
+
+function defaultShouldEmitUrlChange(oldPath: string, newPath: string) {
+    return !!(oldPath && newPath)
+}
+
+export default function createUrlchangeObservable(options: Options = {}): Observable {
+    const {
+        shouldEmitUrlChange = defaultShouldEmitUrlChange,
+        trackReplaceState = true,
+    } = options
+    return new Observable((observer: Observer) => {
+        let path = getPath()
+    
+        function pushStateOverride(originalMethod: Function) {
+            return (...args: any[]) => {
+                originalMethod(...args)
+                handler(true)
+            }
+        }
+    
+        function replaceStateOverride(originalMethod: Function) {
+            return (...args: any[]) => {
+                originalMethod(...args)
+                handler(false)
+            }
+        }
+        
+        function handlePopState() {}
+        
+        function handler(historyDidUpdate: Boolean) {
+            setTimeout(() => {
+                const newPath = getPath()
+                if (path !== newPath && shouldEmitUrlChange(path, newPath)) {
+                    path = newPath
+
+                    if (historyDidUpdate || trackReplaceState) {
+                        observer.next({ page: path })
+                    }
+                }
+            })
+        }
+    
+        MethodChain.add(history, 'pushState', pushStateOverride)
+        MethodChain.add(history, 'replaceState', replaceStateOverride)
+        window.addEventListener('popstate', handlePopState)
+    
+        return () => {
+            MethodChain.remove(history, 'pushState', pushStateOverride)
+            MethodChain.remove(history, 'replaceState', replaceStateOverride)
+            window.removeEventListener('popstate', handlePopState)
+        }
+    })
+}
